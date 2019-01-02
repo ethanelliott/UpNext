67cc3b5b7e741b3597265ac20dc333774a31e29a
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-01-02 13:24:18 -0500

    You bet I just committed the api key

--- front/src/views/PartyView.vue
+++ front/src/views/PartyView.vue
@@ -174,14 +174,14 @@
                         axios.get(`https://musicbrainz.org/ws/2/recording/?fmt=json&limit=10&query=isrc:${t.isrc}`).then(artist_id => {
                             if (artist_id.data.recordings.length > 0) {
                                 let artistID = artist_id.data.recordings[0]["artist-credit"][0].artist.id
-                                axios.get(`https://webservice.fanart.tv/v3/music/${artistID}?api_key=caac10e56d42dd9f2acb2dc924e62c30`).then(images => {
+                                axios.get(`https://webservice.fanart.tv/v3/music/${artistID}?api_key=a71da8227d775ecbfc7e50c9fc87ef37`).then(images => {
                                     if (images.data.artistbackground) {
                                         t.loadBackgroundImage(images.data.artistbackground[Math.floor(Math.random() * images.data.artistbackground.length)].url)
                                     } else {
                                         axios.get(`https://musicbrainz.org/ws/2/recording/?fmt=json&limit=10&query=${t.artist}`).then(artist_id_name => {
                                             if (artist_id_name.data.recordings.length > 0) {
                                                 let artistID = artist_id_name.data.recordings[0]["artist-credit"][0].artist.id
-                                                axios.get(`https://webservice.fanart.tv/v3/music/${artistID}?api_key=caac10e56d42dd9f2acb2dc924e62c30`).then(images_Artist => {
+                                                axios.get(`https://webservice.fanart.tv/v3/music/${artistID}?api_key=a71da8227d775ecbfc7e50c9fc87ef37 `).then(images_Artist => {
                                                     if (images_Artist.data.artistbackground) {
                                                         t.loadBackgroundImage(images_Artist.data.artistbackground[Math.floor(Math.random() * images_Artist.data.artistbackground.length)].url)
                                                     }
