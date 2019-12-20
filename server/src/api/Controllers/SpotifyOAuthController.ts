d2d0555ebc084535ec4d89c2ee8e86ea818813eb
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-12-20 01:59:57 -0500

    so much

--- server/src/api/Controllers/SpotifyOAuthController.ts
+++ server/src/api/Controllers/SpotifyOAuthController.ts
@@ -4,20 +4,28 @@ import { env } from "../../env";
 import SpotifyService from "../Services/SpotifyService";
 import AuthAPI from "../Spotify/apis/AuthAPI";
 import WebTokenService from "../Services/WebTokenService";
+import UUIDService from "../Services/UUIDService";
+import AuthenticationService from "../Services/AuthenticationService";
 
 @JsonController('/auth')
 export class SpotifyOAuthController {
     constructor(
         private spotifyService: SpotifyService,
-        private webTokenService: WebTokenService
+        private webTokenService: WebTokenService,
+        private uuidService: UUIDService,
+        private authenticationService: AuthenticationService
     ) {
     }
 
     @Post('/start')
     public startSpotifyAuth(@QueryParam("partyName") partyName: string, @QueryParam("nickName") nickName: string): any {
+        let pid = this.uuidService.new();
         let state = this.webTokenService.generateFrom({
-            partyName, nickName
+            partyName,
+            nickName,
+            pid
         });
+        // store the pid (party id) in a db, so it can be looked up to confirm that it is in queue to be created
         return this.spotifyService
             .getSpotifyAPI()
             .auth
@@ -27,14 +35,18 @@ export class SpotifyOAuthController {
     @Get('/callback')
     @Redirect(`${env.app.front.url}/make/:token`)
     public async oAuthCallback(@QueryParam("code") code: string, @QueryParam("state") state: string): Promise<any> {
-        // this will get the token from spotify, create a new party, and then callback with the token for the admin
         let decodeState = this.webTokenService.verify(state); // do this first so we know its a valid request before calling spotify
+        // do a lookup in a db for the party id from the state token, and confirm is needs to be created
+        // delete the entry in the db so nobody else can create a party with that spotify account
+        // get the actual token from spotify
         let callbackData = await this.spotifyService.getSpotifyAPI().auth.authorizationCode(env.app.spotify.clientId, env.app.spotify.clientSecret, code, env.app.spotify.redirectURI);
         console.log(callbackData, decodeState);
         // TODO: figure out the authentication service... for checking and creating user tokens
-        let x = this.webTokenService.generateFrom({"blah": "blah"}); // this is the admin token
+        let adminToken = this.authenticationService.generateToken(''); // party id from the token
+        // create party in the db and add the admin as a user
+        // tell the user that the party is being made, and give them their token so they can use it
         return {
-            token: x
+            token: adminToken
         };
     }
 }
--- server/src/middleware/AuthenticationSocketMiddleware.ts
+++ server/src/middleware/AuthenticationSocketMiddleware.ts
@@ -3,8 +3,11 @@ import { Middleware, MiddlewareInterface } from "socket-controllers";
 @Middleware()
 export class AuthenticationSocketMiddleware implements MiddlewareInterface {
     public use(socket: any, next: ((err?: any) => any)) {
-        console.log("do something");
+        // when you connect to the server, there should be some kind of authentication
+        let token = socket.handshake.query.token;
+        if (token) {
 
+        }
         next();
     }
 
