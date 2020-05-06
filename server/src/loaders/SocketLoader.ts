8fc4cc7f752ec36306635ae3dd5c963393968791
ethan <ethan.elliott@hotmail.ca>
2020-05-06 16:54:13 -0400

    adding redirect for netlify deploy

--- server/src/loaders/SocketLoader.ts
+++ server/src/loaders/SocketLoader.ts
@@ -3,7 +3,6 @@ import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3t
 
 import { env } from '../env';
 import { log } from "../util/Log";
-import { createSocketServer } from "socket-controllers";
 import { AuthenticationSocketMiddleware } from "../middleware/AuthenticationSocketMiddleware";
 // Controllers
 import { ConnectionController } from "../api/SocketControllers/ConnectionController";
@@ -12,10 +11,17 @@ import { StateController } from "../api/SocketControllers/StateController";
 import { PlaylistController } from "../api/SocketControllers/PlaylistController";
 import { SpotifyController } from "../api/SocketControllers/SpotifyController";
 import { UserController } from "../api/SocketControllers/UserController";
+import { createSocketServer } from "socket-controllers";
 
 export const SocketLoader: MicroframeworkLoader = (settings: MicroframeworkSettings | undefined) => {
     if (settings) {
         log.startup("Loading SocketIO");
+        // const x = settings.getData('express_server');
+        // const socketOnExpress = sio(x);
+        // const socketApp = useSocketServer(socketOnExpress, {
+        //     controllers: [SearchController, ConnectionController, StateController, PlaylistController, SpotifyController, UserController],
+        //     middlewares: [AuthenticationSocketMiddleware],
+        // });
         const socketApp = createSocketServer(env.app.socketPort, {
             controllers: [SearchController, ConnectionController, StateController, PlaylistController, SpotifyController, UserController],
             middlewares: [AuthenticationSocketMiddleware],
