ce16e207e4463ee5c87d17f96544e405b45afbf8
ethan <ethan.elliott@hotmail.ca>
2020-05-12 02:32:40 -0400

    fixing workbox cache issues with netlify __redirect

--- front/vue.config.js
+++ front/vue.config.js
@@ -6,5 +6,15 @@ module.exports = {
         disableHostCheck: true,
         host: '0.0.0.0',
         port: 8080,
+    },
+    pwa: {
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'default',
+        manifestPath: '/assets/site.webmanifest',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            swSrc: 'sw.js',
+            exclude: [/\.map$/, /_redirects/],
+        }
     }
 }
